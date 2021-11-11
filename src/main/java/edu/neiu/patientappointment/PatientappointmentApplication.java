9f02b697f933b2f251c6446625f98128fd2f1a42
Jestin Jose <fjestinjose@neiu.edu>
2021-11-11 08:43:05 -0600

    Initial commit

---
+++ src/main/java/edu/neiu/patientappointment/PatientappointmentApplication.java
@@ -0,0 +1,13 @@
+package edu.neiu.patientappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientappointmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientappointmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/neiu/patientappointment/PatientappointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neiu.patientappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientappointmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
